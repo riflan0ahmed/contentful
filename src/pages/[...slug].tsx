d7cbc5872a71189c55e19ddc5976c012b4bcbe86
riflan <riflan0ahmed@gmail.com>
2023-05-04 10:17:28 +0530

    testing fallback blocking

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -16,7 +16,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false, // can also be true or 'blocking'
+    fallback: "blocking", // can also be true or 'blocking'
   };
 }
 
